1374a27517c8663b210425e154a2313f0a3bc567
Lee seung chan <dltmdcks702@gmail.com>
2022-05-29 19:15:54 +0900

    feat(login): add session login

--- search_page/app/routes/login.tsx
+++ search_page/app/routes/login.tsx
@@ -10,7 +10,7 @@ import {
 } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
-import { login } from "~/utils/session.server";
+import { createUserSession, login } from "~/utils/session.server";
 import stylesUrl from "~/styles/login.css";
 
 export const links: LinksFunction = () => {
@@ -86,20 +86,15 @@ export const action: ActionFunction = async ({
   switch (loginType) {
     case "login": {
       const user = await login({ username, password });
-      console.log({ user });
+
       if (!user) {
         return badRequest({
           fields,
           formError: `Username/Password combination is incorrect`,
         });
       }
-      // login to get the user
-      // if there's no user, return the fields and a formError
-      // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+      return createUserSession(user.id, redirectTo);
+      
     }
     case "register": {
       const userExists = await db.user.findFirst({
