7badb42dc8eb6bd0b5d23d16d7e98e119a61ab99
Lee seung chan <dltmdcks702@gmail.com>
2022-05-29 17:22:24 +0900

    feat(login): apply use login on login

--- search_page/app/routes/login.tsx
+++ search_page/app/routes/login.tsx
@@ -10,6 +10,7 @@ import {
 } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
+import { login } from "~/utils/session.server";
 import stylesUrl from "~/styles/login.css";
 
 export const links: LinksFunction = () => {
@@ -84,6 +85,14 @@ export const action: ActionFunction = async ({
 
   switch (loginType) {
     case "login": {
+      const user = await login({ username, password });
+      console.log({ user });
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Username/Password combination is incorrect`,
+        });
+      }
       // login to get the user
       // if there's no user, return the fields and a formError
       // if there is a user, create their session and redirect to /jokes
