7851bda5e5d81cb24fa0f1e51a07b46acac0d6a0
Lee seung chan <dltmdcks702@gmail.com>
2022-05-29 16:37:38 +0900

    feat(style): add style with link

--- search_page/app/root.tsx
+++ search_page/app/root.tsx
@@ -1,4 +1,8 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import {
+  Links,
+  LiveReload,
+  Outlet,
+} from "@remix-run/react";
 
 export default function App() {
   return (
@@ -6,6 +10,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
