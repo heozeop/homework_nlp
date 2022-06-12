98b7d8d9b1e084c129fd7b57cb77284874291434
Lee seung chan <dltmdcks702@gmail.com>
2022-06-12 15:43:10 +0900

    show genres on items

--- search_page/app/components/movie.tsx
+++ search_page/app/components/movie.tsx
@@ -3,14 +3,17 @@ import type { Movie } from "@prisma/client";
 export function MoviePosterItem({
   movie,
 }: {
-  movie: Movie
+  movie: Movie & {
+    genres: string[];
+}
 }) {
   return (
     <div className="w-full">
       <div className="aspect-[100/148] w-full">
         <img className="object-fill w-full h-full" src={movie.poster} alt={movie.title}></img>
       </div>
-      <p className="w-full text-ellipsis whitespace-nowrap overflow-hidden">{movie.title}</p>
+      <p className="text-xl w-full text-ellipsis whitespace-nowrap overflow-hidden">{movie.title}</p>
+      <p className="text-sm w-full text-ellipsis whitespace-nowrap overflow-hidden">{movie.genres.join(', ')}</p>
     </div>
   );
 }
--- search_page/app/routes/movies/index.tsx
+++ search_page/app/routes/movies/index.tsx
@@ -10,16 +10,38 @@ import type { Movie } from "@prisma/client";
 import { db } from "~/utils/db.server";
 
 import { MoviePosterItem } from "~/components/movie";
-type LoaderData = { randomMovie: Movie[]};
+type LoaderData = { randomMovie: (Movie & {
+    genres: string[];
+})[]};
 
 export const loader: LoaderFunction = async () => {
-  const randomMovie = await db.movie.findMany();
+  const randomMovie = await db.movie.findMany({
+    include: { 
+      genres: { 
+        include: { 
+          genre: {
+            select: {
+              name: true,
+            }
+          },
+        }
+      },
+    }
+  });
   if (!randomMovie || randomMovie.length === 0) {
     throw new Response("No movie to show", {
       status: 404,
     });
   }
-  const data: LoaderData = { randomMovie };
+  const data: LoaderData = {
+    randomMovie: randomMovie.map(movie => {
+      const genres = movie.genres.map(genre => genre.genre.name)
+      return { 
+        ...movie,
+        genres: genres,
+      }
+    })
+  }
   return json(data);
 };
 
@@ -28,7 +50,7 @@ export default function MoviesIndexRoute() {
 
   return (
     <div className="relative overflow-scroll h-full">
-      <div className="h-full overflow-auto grid gap-4 grid-cols-6">
+      <div className="h-full overflow-auto grid gap-4 grid-cols-5">
         {data.randomMovie.map(movie => (
           <MoviePosterItem movie={movie} />
         ))}
