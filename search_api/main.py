facd962e9e540bd70ef26b02983c296a4274b747
Lee seung chan <dltmdcks702@gmail.com>
2022-06-13 10:08:16 +0900

    feat: api deploy setting

--- search_api/main.py
+++ search_api/main.py
@@ -18,11 +18,12 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "https://imsdb-search.fly.dev/",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
